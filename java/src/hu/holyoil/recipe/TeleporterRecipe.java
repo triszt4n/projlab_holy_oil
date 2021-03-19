1bc21f61fe531b4c12fcda3de63032a1d8171847
vercibar <bercike445@gmail.com>
2021-03-19 17:31:22 +0100

    Improved

--- java/src/hu/holyoil/recipe/TeleporterRecipe.java
+++ java/src/hu/holyoil/recipe/TeleporterRecipe.java
@@ -28,18 +28,51 @@ public class TeleporterRecipe implements IRecipe {
         Logger.RegisterObject(billOfMaterial, "bill: BillOfMaterial");
         Logger.RegisterObject(iron1, "iron1: Iron");
         Logger.RegisterObject(iron2, "iron2: Iron");
+        Logger.RegisterObject(uranium, "uranium: Uranium");
+        Logger.RegisterObject(water, "water: Water");
 
+        Logger.Log(this, "Adding iron to " + Logger.GetName(billOfMaterial));
         billOfMaterial.AddMaterial(iron1);
+        Logger.Return();
+
+        Logger.Log(this, "Adding iron to " + Logger.GetName(billOfMaterial));
         billOfMaterial.AddMaterial(iron2);
+        Logger.Return();
+
+        Logger.Log(this, "Adding uranium to " + Logger.GetName(billOfMaterial));
         billOfMaterial.AddMaterial(uranium);
+        Logger.Return();
+
+        Logger.Log(this, "Adding water to " + Logger.GetName(billOfMaterial));
         billOfMaterial.AddMaterial(water);
+        Logger.Return();
+
+        Logger.Log(this, "Getting player storage");
         PlayerStorage storage = iStorageCapable.GetStorage();
+        Logger.Return();
+
         if (storage.HasEnoughOf(billOfMaterial) && storage.GetOneTeleporter() == null) {
+
+            Logger.Log(this, "Removing bill");
             storage.RemoveBill(billOfMaterial);
+            Logger.Return();
+
             TeleportGate t1 = new TeleportGate(), t2 = new TeleportGate();
+            Logger.RegisterObject(t1, "t1: TeleportGate");
+            Logger.RegisterObject(t2, "t2: TeleportGate");
+
+            Logger.Log(this, "Setting pair of " + Logger.GetName(t1));
             t1.SetPair(t2);
+            Logger.Return();
+
+            Logger.Log(this, "Setting pair of " + Logger.GetName(t2));
             t2.SetPair(t1);
+            Logger.Return();
+
+            Logger.Log(this, "Adding teleportgates to storage");
             storage.AddTeleportGatePair(t1, t2);
+            Logger.Return();
+
         }
     }
 
