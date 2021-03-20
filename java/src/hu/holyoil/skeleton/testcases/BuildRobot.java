81fdf03deb454ec1f4bceac4eae55a1cd3735254
Berci <60003402+vercibar@users.noreply.github.com>
2021-03-20 17:30:59 +0100

    Build robot testcase (#176)
    * Boti approved solution
    
    * Build teleportgate test case. Contains PR 173
    
    * projlab_holy_oil.iml is a mistake
    
    * Added Build Robot testcase

---
+++ java/src/hu/holyoil/skeleton/testcases/BuildRobot.java
@@ -0,0 +1,65 @@
+package hu.holyoil.skeleton.testcases;
+
+import hu.holyoil.collection.BillOfMaterial;
+import hu.holyoil.controller.RobotController;
+import hu.holyoil.crewmate.Settler;
+import hu.holyoil.neighbour.Asteroid;
+import hu.holyoil.recipe.RobotRecipe;
+import hu.holyoil.recipe.TeleporterRecipe;
+import hu.holyoil.resource.Coal;
+import hu.holyoil.resource.Iron;
+import hu.holyoil.resource.Uranium;
+import hu.holyoil.resource.Water;
+import hu.holyoil.skeleton.Logger;
+import hu.holyoil.skeleton.TestCase;
+
+public class BuildRobot extends TestCase {
+
+    Settler settler;
+
+    @Override
+    public String Name() {
+        return "Build Robot";
+    }
+
+    @Override
+    protected void load() {
+
+        Asteroid asteroid = new Asteroid();
+        Logger.RegisterObject(this, "TestFixture");
+        Logger.RegisterObject(asteroid, "a: Asteroid");
+        Logger.RegisterObject(RobotController.getInstance(), ": RobotController");
+        settler = new Settler(asteroid);
+        Logger.RegisterObject(settler, "s: Settler");
+        Logger.RegisterObject(settler.GetStorage(), "storage: Storage");
+        Logger.RegisterObject(RobotRecipe.getInstance(), ": RobotRecipe");;
+
+        boolean canBuildRobot = Logger.GetBoolean(this, "Does the Settler have enough materials to build a robot?");
+
+        if (canBuildRobot) {
+
+            BillOfMaterial billOfMaterial = new BillOfMaterial();
+            Iron iron = new Iron();
+            Logger.RegisterObject(iron, "storageIron: Iron");
+            Coal coal = new Coal();
+            Logger.RegisterObject(coal, "storageCoal: Iron");
+            Uranium uranium = new Uranium();
+            Logger.RegisterObject(uranium, "storageUranium: Uranium");
+            billOfMaterial.AddMaterial(iron);
+            billOfMaterial.AddMaterial(coal);
+            billOfMaterial.AddMaterial(uranium);
+            settler.GetStorage().AddBill(
+                    billOfMaterial
+            );
+
+        }
+
+    }
+
+    @Override
+    protected void start() {
+
+        settler.CraftRobot();
+
+    }
+}
