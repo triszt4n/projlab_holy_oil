05f111cab1d98cecfff96c127f7cb6db11a0306a
Berci <60003402+vercibar@users.noreply.github.com>
2021-04-15 16:47:45 +0200

    start game function (#270)
    * needs review
    
    * now prints all neighbours and their teleportgates. current asteroid is already discovered, so it will be printed alongside the neighbours and discovered ones.
    
    * fixed after merge

--- java/src/hu/holyoil/controller/GameController.java
+++ java/src/hu/holyoil/controller/GameController.java
@@ -1,14 +1,17 @@
 package hu.holyoil.controller;
 
+import hu.holyoil.Main;
+import hu.holyoil.crewmate.Settler;
+import hu.holyoil.crewmate.Ufo;
 import hu.holyoil.neighbour.Asteroid;
+import hu.holyoil.repository.AbstractBaseRepository;
 import hu.holyoil.repository.AsteroidRepository;
 import hu.holyoil.repository.ResourceBaseRepository;
 import hu.holyoil.repository.SettlerRepository;
 import hu.holyoil.resource.*;
 import hu.holyoil.commandhandler.Logger;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * A játékmenetet kezelő singleton osztály. Implementálja az ISteppable interfacet, amivel a köröket kezeli
@@ -32,6 +35,16 @@ public class GameController implements ISteppable  {
     public GameState GetGameState() {
         return gameState;
     }
+
+    /**
+     * A játékban lévő játékosok száma. Ennyivel inicializálódik a játék StartGame() hívásakor.
+     * */
+    public int numOfPlayers = 3;
+
+    /**
+     * A játékban lévő ufo-k száma a játék kezdetekor.
+     * */
+    public int numOfUfos = 3;
 
     /**
      * Lépteti a köröket
@@ -173,12 +186,213 @@ public class GameController implements ISteppable  {
         Logger.Return();
     }
 
+    static int minAsteroidCount = 100;
+    static int maxAsteroidCount = 200;
+    static int minLayerCount = 3;
+    static int maxLayerCount = 5;
+    // chance of asteroids becoming neighbours (%)
+    static int chanceOfNeighbours = 10;
+
     /**
      * elindítja a játékot.
      */
     public void StartGame()  {
         Logger.Log(this,"Starting game");
         // todo
+        // Generate between minAsteroidCount and maxAsteroidCount asteroids
+        Random random = new Random();
+        int numOfAsteroids = minAsteroidCount;
+        if (Main.isRandomEnabled) {
+            numOfAsteroids += random.nextInt(maxAsteroidCount - minAsteroidCount + 1);
+        }
+
+        String startingAsteroidName = null;
+        String ufoStartingAsteroidName = null;
+
+        for (int i = 0; i < numOfAsteroids; i++) {
+
+            Asteroid asteroid = new Asteroid();
+
+            // settlers start at the first asteroid
+            if (startingAsteroidName == null)
+                startingAsteroidName = asteroid.GetId();
+
+            // ufos start at the last asteroid
+            ufoStartingAsteroidName = asteroid.GetId();
+
+            // this looked ugly in "one" line.
+            if (Main.isRandomEnabled) {
+
+                asteroid.SetNumOfLayersRemaining(
+                        random.nextInt(maxLayerCount - minLayerCount) + minLayerCount
+                );
+
+            } else {
+
+                asteroid.SetNumOfLayersRemaining(
+                        (i % (maxLayerCount - minLayerCount + 1)) + minLayerCount
+                );
+
+            }
+
+            asteroid.SetIsDiscovered(false);
+            int generatedResource;
+
+            if (Main.isRandomEnabled) {
+
+                // We generate a resource. There are 4 resources, 0 means no resource.
+                generatedResource = random.nextInt(5);
+
+            } else {
+
+                // With layers being between 3-5, and this being between 0-4, their smallest common divisor is 1.
+                // Meaning that they will generate every combination of layercount and resource.
+                generatedResource = i % 5;
+
+            }
+            AbstractBaseResource resource = null;
+
+            switch (generatedResource) {
+                case 1: {
+                    resource = new Coal(AbstractBaseRepository.GetIdWithPrefix("Coal"));
+                    break;
+                }
+                case 2: {
+                    resource = new Iron(AbstractBaseRepository.GetIdWithPrefix("Iron"));
+                    break;
+                }
+                case 3: {
+                    resource = new Uranium(AbstractBaseRepository.GetIdWithPrefix("Uranium"));
+                    break;
+                }
+                case 4: {
+                    resource = new Water(AbstractBaseRepository.GetIdWithPrefix("Water"));
+                    break;
+                }
+                default: {
+                    // do nothing, this asteroid has no resource
+                    break;
+                }
+            }
+
+            asteroid.SetResource(resource);
+
+        }
+
+        boolean isTraversable = false;
+
+        // we only exit the creation if there are no asteroids without neighbours
+        while (!isTraversable) {
+
+            List<String> asteroidNames = new ArrayList<>();
+            AsteroidRepository.GetInstance().GetAll().forEach(
+                    asteroid -> asteroidNames.add(asteroid.GetId())
+            );
+
+            for (int i = 0; i < asteroidNames.size(); i++) {
+
+                if (Main.isRandomEnabled) {
+
+                    for (int j = 0; j < asteroidNames.size(); j++) {
+
+                        if (i == j) {
+                            break;
+                        }
+
+                        if (random.nextInt(100) < chanceOfNeighbours) {
+
+                            AsteroidRepository.GetInstance().Get(asteroidNames.get(i)).AddNeighbourAsteroid(
+                                    AsteroidRepository.GetInstance().Get(asteroidNames.get(j))
+                            );
+                            AsteroidRepository.GetInstance().Get(asteroidNames.get(j)).AddNeighbourAsteroid(
+                                    AsteroidRepository.GetInstance().Get(asteroidNames.get(i))
+                            );
+
+                        }
+
+                    }
+
+                } else {
+
+                    int j = i + 1;
+                    // if [j] would be out of bounds, we set it to 0
+                    if (j == asteroidNames.size()) {
+                        j = 0;
+                    }
+
+                    // we add chanceOfNeighbours amount of neighbours to every asteroid
+                    while (j <= chanceOfNeighbours + i) {
+
+                        AsteroidRepository.GetInstance().Get(asteroidNames.get(i)).AddNeighbourAsteroid(
+                                AsteroidRepository.GetInstance().Get(asteroidNames.get(j))
+                        );
+                        AsteroidRepository.GetInstance().Get(asteroidNames.get(j)).AddNeighbourAsteroid(
+                                AsteroidRepository.GetInstance().Get(asteroidNames.get(i))
+                        );
+
+                        j++;
+                        // if [j] would be out of bounds, we set it to 0
+                        if (j == asteroidNames.size()) {
+                            j = 0;
+                        }
+
+                    }
+
+                }
+
+            }
+
+            // We traverse the created graph using a BFS.
+            Set<String> toVisit = new HashSet<>();
+            Set<String> visited = new HashSet<>();
+            toVisit.add(startingAsteroidName);
+
+            while (toVisit.size() > 0) {
+
+                String first = (String) toVisit.toArray()[0];
+                toVisit.remove(first);
+
+                for (Asteroid asteroid: AsteroidRepository.GetInstance().Get(first).GetNeighbours()) {
+
+                    if (!visited.contains(asteroid.GetId()))
+                        toVisit.add(asteroid.GetId());
+
+                }
+
+                visited.add(first);
+
+            }
+
+            // we only break if the graph is traversable
+            if (visited.size() == AsteroidRepository.GetInstance().GetAll().size()) {
+
+                isTraversable = true;
+
+            }
+
+        }
+
+        for (int i = 0; i < numOfPlayers; i++) {
+
+            // We generate numOfPlayers amount of settlers on the settler asteroid
+            Settler settler = new Settler(
+                    AsteroidRepository.GetInstance().Get(startingAsteroidName)
+            );
+
+        }
+
+        // the starting asteroid is discovered because there are settlers on it
+        AsteroidRepository.GetInstance().Get(startingAsteroidName).SetIsDiscovered(true);
+
+        for (int i = 0; i < numOfUfos; i++) {
+
+            // we generate numOfUfos amount of ufos on the ufo asteroid
+            Ufo ufo = new Ufo(
+                    AsteroidRepository.GetInstance().Get(ufoStartingAsteroidName)
+            );
+
+        }
+
         Logger.Return();
     }
 
