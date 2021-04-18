6837c91595cef73f437faa42119af5ca72b2aa94
captEmma <75539705+captEmma@users.noreply.github.com>
2021-04-18 15:35:04 +0200

    IIdentifiable comments (#294)

--- java/src/hu/holyoil/IIdentifiable.java
+++ java/src/hu/holyoil/IIdentifiable.java
@@ -1,7 +1,13 @@
 package hu.holyoil;
 
+/**
+ * Az objektumok egyéni nevének kiírásáért felelős interfész.
+ */
 public interface IIdentifiable {
-
+    /**
+     * Minden objektumot egyedileg azonosít.
+     * @return String: az egyed azonosítója
+     */
     String GetId();
 
 }
